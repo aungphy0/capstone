8b25831761271cf116fad07ba4adc72934b9630d
aungphyo88 <aphyo@mail.sfsu.edu>
2023-03-18 17:47:51 -0700

    add some

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,22 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 class Main extends React.Component {
 
     render() {
         return(
             <div>
-                <p>
-
-
-
-
-
-
-
-                    
-                </p>
+            <Card className="main_card">
+                <Card.Body>
+                    <Card.Title>Little Lemon</Card.Title>
+                    <Card.Text>
+                        Welcome to Little Lemon Restaurant.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card.Img className="main_card_img" src="main_card_img.png" />
             </div>
         );
     }
