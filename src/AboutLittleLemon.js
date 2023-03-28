0dfb11b28752bae76d8d8227c3af3f343a64f6df
aungphyo88 <aphyo@mail.sfsu.edu>
2023-03-28 12:35:28 -0700

    changes in bookingform

--- src/AboutLittleLemon.js
+++ src/AboutLittleLemon.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 const AboutLittleLemon = () => {
 
+    const [count, setCount] = useState(0);
+
+    function handlePlusOne(){
+        setCount(count + 1);
+    }
+
+    function handleMinusOne(){
+        setCount(count - 1);
+    }
+
+    function handleReset(){
+        setCount(0);
+    }
     return(
-        <h1>AboutLittleLemon Page</h1>
+        <div className="about-page">
+            <h1>AboutLittleLemon Page</h1>
+
+            <h3>{count}</h3>
+
+            <button onClick={handlePlusOne}>Plus</button>
+            <button onClick={handleMinusOne}>Minus</button>
+            <button onClick={handleReset}>Reset</button>
+        </div>
     );
 };
 
--- src/BookingForm.js
+++ src/BookingForm.js
@@ -1,31 +1,75 @@
+import { useState } from "react";
 import React from "react";
 
 const BookingForm = () => {
+    const [formValue, setFormValue] = useState({
+        date: "",
+        availableTimes: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"],
+        totalGuest: "",
+        occasion: "",
+    });
+
+    function handleFormDate(e1){
+        setFormValue( value => {
+            return ({
+                ...value,
+                date: e1.target.value,
+            });
+        });
+    }
+
+    function handleFormTime(e2){
+        setFormValue( value => {
+            return ({
+                ...value,
+                time: e2.target.value,
+            });
+        });
+    }
+
+    function handleFormGuest(e3){
+        setFormValue( value => {
+            return ({
+                ...value,
+                totalGuest: e3.target.value,
+            });
+        });
+    }
+
+    function handleFormOccasion(e4){
+        setFormValue( value => {
+            return ({
+                ...value,
+                occasion: e4.target.value,
+            });
+        });
+    }
+
+    console.log("date : " + formValue.date);
+    console.log("time : " + formValue.time);
+    console.log("guests : " + formValue.totalGuest);
+    console.log("occasion : " + formValue.occasion);
 
     return (
-        
         <form className="booking-form">
             <label for="res-date">Choose date</label>
-            <input type="date" id="res-date"/>
+            <input type="date" id="res-date" onChange={handleFormDate}/>
             <label for="res-time">Choose time</label>
-            <select id="res-time ">
-                <option>17:00</option>
-                <option>18:00</option>
-                <option>19:00</option>
-                <option>20:00</option>
-                <option>21:00</option>
-                <option>22:00</option>
+            <select id="res-time " onChange={handleFormTime}>
+                {formValue.availableTimes.map( time => {
+                    return <option>{time}</option> })
+
+                }
             </select>
             <label for="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests"/>
+            <input type="number" placeholder="1" min="1" max="10" id="guests" onChange={handleFormGuest}/>
             <label for="occasion">Occasion</label>
-            <select id="occasion">
+            <select id="occasion" onChange={handleFormOccasion}>
                 <option>Birthday</option>
                 <option>Anniversary</option>
             </select>
             <input type="submit" value="Make Your reservation"/>
         </form>
-        
     );
 };
 
