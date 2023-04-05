64cc5b4a34efc44b985ff54c3a1c3b4f15aa9a0d
aungphyo88 <aphyo@mail.sfsu.edu>
2023-04-04 21:30:10 -0700

    update readme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import BookingForm from './BookingForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('booking form test', () => {
+  render(<BookingForm />);
+  const date = screen.getByLabelText(/Choose date/);
+  expect(date).toBe("");
 });
